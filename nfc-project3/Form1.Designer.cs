a9308dc070b8f8c9c74fd28cdb6942e718a7c183
alucard <faalsaru@gmail.com>
2022-04-05 20:36:47 -0500

    Add project files.

---
+++ nfc-project3/Form1.Designer.cs
@@ -0,0 +1,229 @@
+﻿
+namespace nfc_project3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.DeviceCountLabel = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Texto = new System.Windows.Forms.TextBox();
+            this.WriteButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.StartButton = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.DeviceCountLabel);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.Texto);
+            this.panel1.Controls.Add(this.WriteButton);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.StartButton);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(776, 100);
+            this.panel1.TabIndex = 0;
+            // 
+            // DeviceCountLabel
+            // 
+            this.DeviceCountLabel.AutoSize = true;
+            this.DeviceCountLabel.Location = new System.Drawing.Point(104, 66);
+            this.DeviceCountLabel.Name = "DeviceCountLabel";
+            this.DeviceCountLabel.Size = new System.Drawing.Size(13, 13);
+            this.DeviceCountLabel.TabIndex = 7;
+            this.DeviceCountLabel.Text = "0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(186, 25);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(123, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Digite algún texto o URL";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(514, 53);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(128, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "si desea escribir una URL";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(514, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(205, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "NOTA: Asegurese de agregar el protocolo";
+            // 
+            // Texto
+            // 
+            this.Texto.Location = new System.Drawing.Point(189, 41);
+            this.Texto.Multiline = true;
+            this.Texto.Name = "Texto";
+            this.Texto.Size = new System.Drawing.Size(154, 38);
+            this.Texto.TabIndex = 3;
+            // 
+            // WriteButton
+            // 
+            this.WriteButton.Location = new System.Drawing.Point(349, 48);
+            this.WriteButton.Name = "WriteButton";
+            this.WriteButton.Size = new System.Drawing.Size(75, 23);
+            this.WriteButton.TabIndex = 2;
+            this.WriteButton.Text = "Write Button";
+            this.WriteButton.UseVisualStyleBackColor = true;
+            this.WriteButton.Click += new System.EventHandler(this.WriteButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "# Adaptadores";
+            // 
+            // StartButton
+            // 
+            this.StartButton.Location = new System.Drawing.Point(24, 40);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(75, 23);
+            this.StartButton.TabIndex = 0;
+            this.StartButton.Text = "Start Button";
+            this.StartButton.UseVisualStyleBackColor = true;
+            this.StartButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.splitContainer1);
+            this.panel2.Location = new System.Drawing.Point(12, 118);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(776, 320);
+            this.panel2.TabIndex = 1;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.textBox2);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.webBrowser1);
+            this.splitContainer1.Size = new System.Drawing.Size(776, 320);
+            this.splitContainer1.SplitterDistance = 258;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(0, 0);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(255, 320);
+            this.textBox2.TabIndex = 4;
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.Location = new System.Drawing.Point(0, 0);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(514, 320);
+            this.webBrowser1.TabIndex = 0;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button StartButton;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox Texto;
+        private System.Windows.Forms.Button WriteButton;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Label DeviceCountLabel;
+    }
+}
+
---
+++ nfc-project3/Form1.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;// UTILIZADO PARA LAS EXPRESIONES REGULARES
+using NFC_Wrapper_Sample;
+
+namespace nfc_project3
+{
+    public partial class Form1 : Form
+    {
+        [System.Runtime.InteropServices.DllImport("kernel32.dll", EntryPoint = "LoadLibraryA")]
+        static extern int LoadLibrary(string lpLibFileName); // CARGAMOS LA LIBRERIA
+
+        private TNFCWrapper NFCWrapper;
+
+        private string Mensaje = null; //VARIABLE MENSAJE GLOBAL
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public static bool ValidateUrl(string url) // METODO CON EXPRESION REGULAR QUE VALIDA LAS URL
+        {
+            if (url == null || url == "") return false;
+            Regex oRegExp = new Regex(@"(http|ftp|https)://([\w-]+\.)+(/[\w- ./?%&=]*)?", RegexOptions.IgnoreCase);
+            return oRegExp.Match(url).Success;
+        }
+
+        private void LogMessage(string Msg) // FORMATO DE MENSAJES
+        {
+            textBox2.Text += Msg;
+            textBox2.Text += Environment.NewLine;
+            textBox2.Refresh();
+            textBox2.SelectionStart = textBox2.Text.Length;
+            textBox2.ScrollToCaret();
+        }
+
+        [System.Security.Permissions.PermissionSet(System.Security.Permissions.SecurityAction.Demand, Name = "FullTrust")] // PERMISOS
+        protected override void WndProc(ref Message aMessage) // TIPOS DE MENSAJES Y EVENTOS
+        {
+            if (aMessage.Msg == TNFCWrapper.WM_NFC_NOTIFY)
+            {
+                string s = "";
+                Int32 wParam = aMessage.WParam.ToInt32();
+                switch (wParam)
+                {
+                    case TNFCWrapper.NFC_NDEF_FOUND:
+                        s = "NFC_NDEF_FOUND Size = " +
+                       aMessage.LParam.ToString();
+                        break;
+                    case TNFCWrapper.NFC_DEVICE_CHANGED:
+                        s = "Dispositivo NFC Cargado";
+                        break;
+                    case TNFCWrapper.NFC_UNKNOWN_SERVICE:
+                        s = "Servicio NFC desconocido";
+                        break;
+                    case TNFCWrapper.NFC_CONNECTED:
+                        s = "Tag NFC Conectado";
+                        break;
+                    case TNFCWrapper.NFC_DISCONNECTED:
+                        s = "NFC Desconectado";
+                        break;
+                    case TNFCWrapper.NFC_IDLE:
+                        s = "NFC_IDLE";
+                        break;
+                    default:
+                        s = "Desconocido";
+                        break;
+                }
+                LogMessage(s);
+                if (wParam == TNFCWrapper.NFC_DEVICE_CHANGED)
+                    DeviceCountLabel.Text = aMessage.LParam.ToString();
+                if (wParam == TNFCWrapper.NFC_NDEF_FOUND)
+                    ReadNDEF();
+            }
+            base.WndProc(ref aMessage);
+        }
+
+        private void ReadNDEF()
+        {
+            UInt32 DeviceCount = 0;
+            UInt32 MessageCount = 0;
+            UInt32 NextMessageSize = 0;
+            UInt32 Result;
+            if (NFCWrapper == null) return;
+            // Get information about the message queue
+            Result = TNFCWrapper.GetNDEFQueueInfo(ref DeviceCount, ref MessageCount, ref NextMessageSize);
+            LogMessage("GetNDEFQueueInfo: " + NFCWrapper.NFCWrapperErrorToString(Result));
+
+            if (Result != TNFCWrapper.ERR_SUCCESS) return;
+
+            LogMessage(" DeviceCount = " + DeviceCount.ToString());
+
+            LogMessage(" MessageCount = " + MessageCount.ToString());
+
+            LogMessage(" NextMessageSize = " + NextMessageSize.ToString());
+
+            //Resize the NDEF buffer accordingly to the site of the next message in the queue
+
+            byte[] NDEF = new byte[NextMessageSize];
+            UInt32 NDEFSize = NextMessageSize;
+            TNFCAddress NFCAddress = new TNFCAddress();
+            TMessageInfo MessageInfo = new TMessageInfo();
+
+            //Read the NDEF message from the message queue
+            Result = TNFCWrapper.ReadNDEF(ref NFCAddress, ref
+            MessageInfo, ref NDEF[0], ref NDEFSize);
+            LogMessage("ReadNDEF: " + NFCWrapper.NFCWrapperErrorToString(Result));
+            if (Result != TNFCWrapper.ERR_SUCCESS) return;
+            
+            // CONVERTIR A XML
+            string XML = "";
+            string XML1 = null;
+            string XML2 = null;
+            Result = NFCWrapper.NDEF2XML(ref NDEF[0], NDEFSize, ref XML);
+            LogMessage("NDEF2XML: " + NFCWrapper.NFCWrapperErrorToString(Result));
+            if (Result != TNFCWrapper.ERR_SUCCESS) return;
+            // print NDEF as XML
+            textBox2.Clear();
+            LogMessage(XML);
+            // PRIMER FILTRADO del string donde, tomaremos linea por linea el string XML y tomamos como delimitador los saltos de linea
+             // es alli donde se encuentra la linea del mensaje o url. este es el resultado: <NDEF_URI:URI>Mensaje</NDEF_URI:URI>
+             XML1 = XML.Split('\n')[12];
+             // SEGUNDO FILTRADO del string anterior donde, tomaremos linea por linea el string XML1, y tomamos como delimitador ">"
+             // este es el resultado en la posicion 1: <NDEF_URI:URI>Mensaje
+             XML2 = XML1.Split('<')[1];
+             // ULTIMO FILTRADO del string anterior donde, tomaremos linea por linea el string XML2, y tomamos como delimitador ">"
+             // este es el resultado en la posicion 1 : Mensaje
+             Mensaje = XML2.Split('>')[1];
+             if (ValidateUrl(Mensaje) == true) // VALIDAMOS QUE SI MENSAJE ES UNA URL ENTONCES
+             {
+                 webBrowser1.Navigate(Mensaje); // ENVIAR URL AL NAVEGADOR Y EJECUTARSE
+             }
+             else
+             {
+             MessageBox.Show(Mensaje, "Mensaje"); // SINO ES UNA URL ENTONCES ENVIAR ALERTA CON EL MENSAJE
+             }
+             LogMessage(" ");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Verificar que exista la libreria
+            if (LoadLibrary("SCM_NFC.dll") == 0)
+            {
+                LogMessage("NFC Wrapper no encontrada");
+                LogMessage("Asegurate que el archivo SCM_NFC.dll este presente");
+                return;
+            }
+            LogMessage("SCM_NFC.DLL Cargada exitosamente.");
+            StartButton.Enabled = false;
+            WriteButton.Enabled = true; // SE HABILITA EL BOTON PARA ESCRIBIR EN LA TARJETA
+            //Init NFC Wrapper
+            
+            NFCWrapper = new TNFCWrapper();
+            TNFCWrapper.Initialize((UInt32)Handle.ToInt32());
+            //DECIRLE A NFC WRAPPER QUE EMPIECE CON LA LECTURA
+            TNFCWrapper.StartListening();
+            LogMessage("Por favor coloca una tag NFC");
+        }
+
+        private void WriteButton_Click(object sender, EventArgs e)
+        {
+            if (NFCWrapper == null) return;
+            UInt32 Result;
+            //Create Smart Poster NDEF Message
+            // INFORMACION NECESARIO PARA ENVIARLE A TNFCWRAPPER.CS COMO PARAMETRO
+            string URI = Texto.Text.ToString();
+            string Comment = "";
+            string Language = "en-US";
+            string TargetType = "";
+            UInt32 Size = 0;
+            byte Action = 0;
+            UInt32 NDEFSize = 1000;
+            byte[] NDEF = new byte[NDEFSize];
+            Result = TNFCWrapper.CreateNDEFSp(URI, Comment, Language, ref Action, ref Size, TargetType, ref NDEF[0], ref NDEFSize);
+            LogMessage("CreateNDEFSp: " + NFCWrapper.NFCWrapperErrorToString(Result));
+            if (Result != TNFCWrapper.ERR_SUCCESS) return;
+            // convert NDEF into XML
+            string XML = "";
+            Result = NFCWrapper.NDEF2XML(ref NDEF[0], NDEFSize, ref XML);
+            LogMessage("NDEF2XML: " + NFCWrapper.NFCWrapperErrorToString(Result));
+            if (Result != TNFCWrapper.ERR_SUCCESS) return;
+            // print NDEF as XML
+            LogMessage(XML);
+            LogMessage(" ");
+            // ESCRIBIMOS EL NDEF A LA ETIQUETA
+            LogMessage("Ponga una NFC Tag en los proximos 5 segundos...");
+
+            Texto.Clear();
+            TNFCAddress NFCAddress = new TNFCAddress();
+            TMessageInfo MessageInfo = new TMessageInfo();
+            Result = TNFCWrapper.WriteNDEF(ref NFCAddress, ref MessageInfo, ref NDEF[0], ref NDEFSize, false, true, 5);
+            LogMessage("NDEF2XML: " + NFCWrapper.NFCWrapperErrorToString(Result));
+            LogMessage("");
+        }
+
+    }
+}
